a55cd08820856b3f597bd05f842767db3ab4722d
wcy <1667718931@qq.com>
2023-10-24 08:19:09 +0800

    second

---
+++ app/src/main/java/au/edu/federation/itech3107/studentattendance30395569/util/bc.java
@@ -0,0 +1,40 @@
+package au.edu.federation.itech3107.studentattendance30395569.util;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃  神兽保佑
+ * 　　　　┃　　　┃  代码无bug
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ *
+ * @author
+ * @date 2023/10/19
+ * @description
+ */
+
+public class bc {
+    public static final String INTENT_EDIT_COURSE = "intent_course";
+    public static final String[] WEEK_SINGLE = {"一", "二", "三", "四", "五", "六", "日"};
+    public static final String[] WEEK = {"", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    public static final String INTENT_ADD_COURSE_ANCESTOR = "intent_add_course_ancestor";
+    public static final String INTENT_ADD = "intent_course_add";
+    public static  String select_class ="";
+    public static  String select_student ="";
+    public static int selectId;
+    public static final String DEFAULT_ALL_WEEK = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25";
+
+}
